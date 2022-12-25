4caa610bd9e48d1a34007a2670b6348048906ba6
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-25 23:04:29 +0530

    [amol]- Mood Analyser Problems

---
+++ MoodAnayser/MoodAnayser/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MoodAnayser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodSadOrHappy obj = new MoodSadOrHappy();
+            obj.Mood("happy");
+
+        }
+    }
+}

e61c81a6774374d5148bba16e1c3b80ab4b67c41
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-25 23:47:32 +0530

    [amol]-  Give Mood Should Return Happy Mood

--- MoodAnayser/CheckMood.UnitTest/UnitTest1.cs
+++ MoodAnayser/CheckMood.UnitTest/UnitTest1.cs
@@ -16,5 +16,15 @@ namespace CheckMood.UnitTest
             string expected = "Sad";
             Assert.AreEqual(expected, AcutalResult);
         }
+        [TestMethod]
+        public void GiveMoodShouldReturnHappMood()
+        {
+            const string happymood = "Happy";
+            const string sadmood = "Sad";
+            MoodSadOrHappy mood = new MoodSadOrHappy();
+            string AcutalResult = mood.Mood(happymood);
+            string expected = "Happy";
+            Assert.AreEqual(expected, AcutalResult);
+        }
     }
 }
